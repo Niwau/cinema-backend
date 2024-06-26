bdd2c101b0d57e3a258bd04b738a7b52b9fb1014
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2024-06-26 19:46:20 -0300

    fix: database

---
+++ Migrations/20240626221641_Tickets.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cinema_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tickets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerEmail = table.Column<string>(type: "TEXT", nullable: false),
+                    SessionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChairId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -3,7 +3,7 @@ namespace cinema_backend.Models
     public class Ticket
     {
         public int Id { get; set; }
-        public required string CustomerEmail { get; set; }
+        public string CustomerEmail { get; set; }
         public int SessionId { get; set; }
         public int ChairId { get; set; }
     }
